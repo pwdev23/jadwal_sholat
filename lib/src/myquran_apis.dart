31ceef7219125cfc8b2991443531bba86a5163c7
iceDViz <mirhasalhafidz@gmail.com>
2023-09-27 06:58:51 +0700

    Added getAllCities and it test

--- lib/src/myquran_apis.dart
+++ lib/src/myquran_apis.dart
@@ -9,6 +9,20 @@ import 'utils.dart';
 const kBaseUrl = 'https://api.myquran.com';
 
 class MyQuranApis {
+  Future<List<City>> getAllCities() async {
+    const path = '/v1/sholat/kota/semua';
+
+    final url = Uri.parse('$kBaseUrl$path');
+
+    final res = await http.get(url);
+
+    if (res.statusCode != 200) throw Exception('Failed to load');
+
+    final d = json.decode(res.body);
+
+    return (d as List).map((e) => City.fromJson(e)).toList();
+  }
+
   Future<List<City>> getCities(
     String cityName,
   ) async {
--- test/myquran_apis_test.dart
+++ test/myquran_apis_test.dart
@@ -1,20 +1,32 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:jadwal_sholat/src/models/city.dart';
 
 import 'package:jadwal_sholat/src/myquran_apis.dart';
 
 void main() {
   final api = MyQuranApis();
   final someday = DateTime(2023, 9, 23, 17, 30);
-  test('Get schedule test', () async {
-    late String fajr;
 
-    final prayerScedule = await api.getPrayerSchedulePerDay(
-      cityId: '1609',
-      date: someday,
-    );
+  group('MyQuran APIs test', () {
+    test('Get all cities test', () async {
+      late String id;
+      List<City> cities = <City>[];
+      cities = await api.getAllCities();
+      id = cities[0].id;
+      expect(id, '0101');
+    });
 
-    fajr = prayerScedule.fajr;
+    test('Get schedule test', () async {
+      late String fajr;
 
-    expect(fajr, '04:06');
+      final prayerScedule = await api.getPrayerSchedulePerDay(
+        cityId: '1609',
+        date: someday,
+      );
+
+      fajr = prayerScedule.fajr;
+
+      expect(fajr, '04:06');
+    });
   });
 }
